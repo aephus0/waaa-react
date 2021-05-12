1ac2f93b42c965c8bfd74f3d22db91e21c1b1162
xukkz <wiggo.bergman@gmail.com>
2021-05-12 12:27:15 +0200

    waa

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React, { useState } from "react";
+import "./styles.css";
+const url = "https://aniliapi.herokuapp.com/";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      {" "}
+      {}
+      <div>
+        <form
+          action="https://localhost:5500/user/avatar/upload"
+          method="post"
+          enctype="multipart/form-data"
         >
-          Learn React
-        </a>
-      </header>
+          <input type="file" name="avatar" />
+        </form>
+      </div>
+      <AnimeInfo
+        title="Attack on Titan"
+        description="Anime about slaying titans"
+        releasedate={Date.now()}
+        aniId={1234}
+      />
+      <AnimeInfo
+        title="Attack on Titan Season 2"
+        description="Anime about slaying titans once again"
+        releasedate={Date.now()}
+        aniId={1243}
+      />
+      <AnimeInfo
+        title="Clannad"
+        description="An amazing anime about a group of teenagers."
+        releasedate={Date.now()}
+        aniId={4321}
+      />
+    </>
+  );
+};
+
+const AnimeInfo = (props) => {
+  const [showAniId, setAniId] = useState(false);
+  return (
+    <div className="aniEntry">
+      <img src="https://cdn.lewd.host/nhGyUn4L.png" alt="anime"></img>
+      <div className="aniDetails">
+        <p>Title: {props.title}</p>
+        <p>Description: {props.description}</p>
+        <p>Release date: {props.releasedate}</p>
+        <button onClick={() => setAniId(!showAniId)}>Toggle aniId</button>
+        {showAniId && <p>aniID: {props.aniId}</p>}
+      </div>
     </div>
   );
-}
+};
 
+const fetchData = async (url, data = {}) => {
+  const response = await fetch(url + "anime", {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  });
+  return response.json();
+};
+
+console.log(
+  fetchData(url).then((data) => {
+    const anidata = data;
+    return anidata;
+  })
+);
 export default App;
